02cbab7054954097002a730f55ebba7f1d337513
Wenbin <wen6inli@gmail.com>
2013-10-25 17:01:20 -0400

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // all environments
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -27,10 +27,7 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
-
-
 app.get('/', routes.index);
-
 
 app.get('/users', function(req, res, next) {
 
@@ -56,7 +53,7 @@ app.get('/users', function(req, res, next) {
 			client.hget("guard_list", reply, function(err, obj) {
 				guards.push({
 					'email': reply,
-					'cp':  obj 
+					'cp': obj
 				});
 				console.log("    " + guards.length + ": " + obj);
 
@@ -64,43 +61,60 @@ app.get('/users', function(req, res, next) {
 					console.log("aaaaaaaa    " + guards.length + " aaaaaaa ");
 					console.log("aaaaaaaa    " + i + " aaaaaaa ");
 
-// var n = 1000;
-// while (n--) {
-//   pets.push({ name: 'Tobi', age: 2, species: 'ferret' });
-//   pets.push({ name: 'Loki', age: 1, species: 'ferret' });
-//   pets.push({ name: 'Jane', age: 6, species: 'ferret' });
-// }
-
-
-					  res.render('user',  { guards: guards });
+					res.render('user', {
+						guards: guards
+					});
 
 				}
 			});
-
-
-
 		});
 
-
-
 		client.quit();
+	});
+});
 
+app.get('/vehciles', function(req, res, next) {
 
+	vehciles = [];
+
+	var redis = require("redis"),
+		client = redis.createClient();
+
+	// if you'd like to select database 3, instead of 0 (default), call
+	// client.select(3, function() { /* ... */ });
+
+	client.on("error", function(err) {
+		console.log("Error " + err);
 	});
 
+	// client.set("string key", "string val", redis.print);
+	// client.hset("hash key", "hashtest 1", "some value", redis.print);
+	// client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+	client.hkeys("vehciles_list", function(err, replies) {
+		console.log(replies.length + " replies:");
+		a = replies.forEach(function(reply, i) {
 
+			client.hget("guard_list", reply, function(err, obj) {
+				guards.push({
+					'email': reply,
+					'cp': obj
+				});
+				console.log("    " + guards.length + ": " + obj);
 
-	// User.count(function(err, count){
-	//   if (err) return next(err);
-	//   User.all(function(err, users){
-	//     if (err) return next(err);
-	//     res.render('user', {
-	//       title: 'Users',
-	//       count: count,
-	//       users: users.filter(ferrets)
-	//     });
-	//   })
-	// })
+				if (i == (replies.length - 1)) {
+					console.log("aaaaaaaa    " + guards.length + " aaaaaaa ");
+					console.log("aaaaaaaa    " + i + " aaaaaaa ");
+
+					res.render('user', {
+						guards: guards
+					});
+
+				}
+			});
+		});
+
+		client.quit();
+	});
 });
 
 http.createServer(app).listen(app.get('port'), function() {
